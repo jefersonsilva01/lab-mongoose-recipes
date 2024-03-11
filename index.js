324c3d23c0b9be5d3b04988d73b3e81c8cac9df5
jefersonsilva01 <jeferson.s.silva1@gmail.com>
2024-03-11 13:42:23 -0300

    Done

--- index.js
+++ index.js
@@ -5,6 +5,26 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
+const myRecipe = {
+  "title": "My Asian Glazed Chicken Thighs",
+  "level": "Amateur Chef",
+  "ingredients": [
+    "1/2 cup rice vinegar",
+    "5 tablespoons honey",
+    "1/3 cup soy sauce (such as Silver Swan®)",
+    "1/4 cup Asian (toasted) sesame oil",
+    "3 tablespoons Asian chili garlic sauce",
+    "3 tablespoons minced garlic",
+    "salt to taste",
+    "8 skinless, boneless chicken thighs"
+  ],
+  "cuisine": "Asian",
+  "dishType": "main_course",
+  "image": "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+  "duration": 40,
+  "creator": "Chef LePapu"
+}
+
 const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
 
 // Connection to the database "recipe-app"
@@ -17,7 +37,30 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe.create(myRecipe)
+      .then(recipe => console.log('The recipe is save and its title is:', recipe.title))
+      .catch(error => console.log('An error happened:', error));
+
+    Recipe.insertMany(data)
+      .then(values => console.log(values))
+      .catch(error => console.log(error))
+      .then(() => {
+        Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+          .then(console.log("Recipe duration updated"))
+          .catch(error => console.log(error));
+      })
+      .then(() => {
+        Recipe.deleteOne({ title: "Carrot Cake" })
+          .then(console.log("Recipe deleted"))
+          .then(() => mongoose.connection.close())
+          .catch(error => console.log(error));
+      })
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+mongoose.connection.on('disconnected', () =>
+  console.log('Mongoose default connection disconnected')
+);
+
